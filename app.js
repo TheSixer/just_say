7a50ac75f2241c976fddeb784e5be7177d08f532
TheSixth <longjia3713@163.com>
2017-03-08 07:27:46 +0800

    UI

--- app.js
+++ app.js
@@ -4,22 +4,7 @@ var config = require('./service/config.js'),
 App({
   onLaunch: function () {
     console.log('App Launch')
-    var logs = wx.getStorageSync('logs') || [],
-        that = this
-    //检查是否已有登录信息
-    wx.getStorage({
-      key: 'userInfo',
-      success: function(res) {
-        
-        if(res.data) {
-          that.globalData.hasLogin = true
-          that.globalData.userInfo = res.data
-        }
-      },
-      fail: function(res) {
-          that.getUserInfo()
-      }
-    })
+    var logs = wx.getStorageSync('logs') || []
   },
   onShow: function () {
     console.log('App Show')
@@ -27,40 +12,6 @@ App({
   },
   onHide: function () {
     console.log('App Hide')
-  },
-  getUserInfo:function(cb){
-    var that = this;
-
-    //调用登录接口
-    wx.login({
-      success: function(res) {
-        _getUserInfo(res.code)
-      }
-    })
-
-    function _getUserInfo(code) {
-      wx.getUserInfo({
-        success: function (res) {
-          //将用户信息添加到全局
-          that.globalData.hasLogin = true
-          that.globalData.userInfo = res.userInfo
-          that.globalData.userInfo.code = code
-          
-          try {
-            wx.setStorageSync('userInfo', that.globalData.userInfo)
-          } catch (e) {    
-            var title = 'tips',
-                tips = '程序异常，请联系客服！'
-            dealErr.showTips(title, tips, function(){})
-          }
-        },
-        fail: function() {
-          var title = 'tips',
-              tips = '获取登录信息失败！'
-          dealErr.showTips(title, tips, function(){})
-        }
-      })
-    }
   },
   globalData: {
     APIUrl: config.config.api
--- page/mine/index.js
+++ page/mine/index.js
@@ -7,17 +7,79 @@ Page({
     userInfo: {},
     nickName: '点击登录'
   },
+  getStorage: function() {
+    var that = this
+    //检查是否已有登录信息
+    wx.getStorage({
+      key: 'userInfo',
+      success: function(res) {
+        
+        if(res.data) {
+          app.globalData.hasLogin = true
+          app.globalData.userInfo = res.data
+
+          that.setData({
+            userInfo: res.data,
+            hasLogin: true
+          })
+        }
+      },
+      fail: function(res) {
+          that.getUserInfo()
+      }
+    })
+  },
+  getUserInfo:function(cb){
+    var that = this;
+
+    //调用登录接口
+    wx.login({
+      success: function(res) {
+        _getUserInfo(res.code)
+      }
+    })
+
+    function _getUserInfo(code) {
+      wx.getUserInfo({
+        success: function (res) {
+          //将用户信息添加到全局
+          var userInfo = res.userInfo
+
+          userInfo.code = code
+
+          app.globalData.hasLogin = true
+          app.globalData.userInfo = userInfo
+          
+          that.setData({
+            userInfo: userInfo,
+            hasLogin: true
+          })
+
+          try {
+            wx.setStorageSync('userInfo', userInfo)
+          } catch (e) {    
+            var title = 'tips',
+                tips = '程序异常，请联系客服！'
+            dealErr.showTips(title, tips, function(){})
+          }
+        },
+        fail: function() {
+          var title = 'tips',
+              tips = '获取登录信息失败！'
+          dealErr.showTips(title, tips, function(){})
+        }
+      })
+    }
+  },
   onLoad: function () {
     var that = this
     //如果用户已登录，获取用户本地信息
-    // that.getStorage()
+    that.getStorage()
     
-    var APIUrl = app.globalData.APIUrl,
-        userInfo = app.globalData.userInfo
+    var APIUrl = app.globalData.APIUrl
 
     that.setData({
-      userInfo: userInfo,
-      hasLogin: true
+      url: APIUrl
     })
 
   },
